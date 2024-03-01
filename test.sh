7c6d5dba90af798b58d6aadfef296b556f8d14e2
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2024-03-01 15:59:18 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 for dataset in deepfashion; do
-    for model in tiny medium large huge; do # tiny large huge
-        for size in 256; do #  384 480 512
+    for model in tiny medium base large huge; do
+        for size in 512; do # 256 384 480 512
             python test.py --dataset_name $dataset --img_size $size --model_size $model
         done
     done
